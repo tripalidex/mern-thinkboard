26a0e0554fd23ee467ef62ba79d16676edf06920
Carlos Martin Quispe <carlosquispe1805@gmail.com>
2025-11-09 08:51:42 -0500

    Implementar middleware y rate limiter

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,24 +3,31 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-connectDB();
-
 // middleware
-app.use(express.json());
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
+
+// our simple custom middleware
+// app.use((req, res, next) => {
+//     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//     next();
+// });
 
 app.use("/api/notes", notesRoutes);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on PORT:", PORT);
+    });
+});
 
 /*
 What is an Endpoint?
 An endpoint is a combination of a URL + HTTP method that lets the client interact with a specific resource
 */
-
-app.listen(PORT, () => {
-    console.log("Server started on PORT:", PORT);
-});
