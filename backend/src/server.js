c9871b0a5330b6b38d71d11ff69b02adf41981e5
Carlos Martin Quispe <carlosquispe1805@gmail.com>
2025-11-09 12:22:02 -0500

    Implementar HomePage
    Se realizo el fetch de las notas para verlas como una lista de cards

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 //const express = require("express");
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
@@ -11,6 +13,9 @@ const app = express();
 const PORT = process.env.PORT;
 
 // middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import NavBar from '../components/NavBar'
+import RateLimitedUI from '../components/RateLimitedUI';
+import axios from "axios";
+import toast from "react-hot-toast";
+import NoteCard from '../components/NoteCard';
 
 const HomePage = () => {
-  return (
-    <div className=''>HomePage</div>
-  )
+    const [isRateLimited, setIsRateLimited] = useState(false);
+    const [notes, setNotes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchNotes = async () => {
+            try {
+                const res = await axios.get("http://localhost:5001/api/notes");
+                console.log(res.data);
+                setNotes(res.data);
+                setIsRateLimited(false);
+            } catch (error) {
+                console.error("Error fetching notes", error);
+                if (error.response?.status === 429) {
+                    setIsRateLimited(true);
+                } else {
+                    toast.error("Failed to load notes")
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchNotes();
+    }, [])
+
+    return (
+        <div className='min-h-screen'>
+            <NavBar />
+
+            {isRateLimited && <RateLimitedUI />}
+
+            <div className='max-w-7xl max-auto p-4 mt-6'>
+                {loading &&
+                    <div className='text-center text-primary py-10'>
+                        Loading notes...
+                    </div>
+                }
+                {notes.length > 0 && !isRateLimited && (
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                        {notes.map(note => (
+                            <NoteCard key={note._id} note={note} />
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default HomePage
