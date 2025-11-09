cfdd424ceb9b1e3a406258e56cff2c455ca3bf1c
Carlos Martin Quispe <carlosquispe1805@gmail.com>
2025-11-08 19:53:48 -0500

    Conectar con la base de datos en MongoDB
    Se crearon las conexiones con MongoDB y el primer model para Note. Además, se utilizan variables de entorno (.env), para asegurar que no sea accesible la conexion con la base de datos y el puerto de la URL de los endpoints.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 //const express = require("express");
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
@@ -11,6 +18,6 @@ What is an Endpoint?
 An endpoint is a combination of a URL + HTTP method that lets the client interact with a specific resource
 */
 
-app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
+app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
 });
